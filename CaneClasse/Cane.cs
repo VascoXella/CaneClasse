bd8df44136924d0d6613b44f7030db51d05a415a
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-22 20:37:41 +0100

    Aggiungere i file di progetto.

---
+++ CaneClasse/Cane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaneClasse
+{
+    internal class Cane
+    {
+        public string Razza { get; set; }
+        public string Nome { get; set; }
+        public Persona Proprietario { get; set; }
+        public Cane()
+        {
+
+        }
+
+    }
+}
---
+++ CaneClasse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaneClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane c = new Cane();
+            c.Nome = "Ringhio";
+            c.Razza = "Labrador";
+
+            c.Proprietario = new Persona();
+            c.Proprietario.Nome = "Rossi";
+
+            Console.WriteLine("{0}, {1}, {2}", c.Nome, c.Razza, c.Proprietario.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
